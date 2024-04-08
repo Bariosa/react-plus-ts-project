ba42d076812e389c7c5bd337a0e169b12a4df67d
Anastasia Bardier <bardieranastasia@gmail.com>
2024-04-08 16:38:51 +0300

    UserStat statistics block for UserCard

---
+++ src/mock/index.ts
@@ -0,0 +1,17 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+  login: "Bariosa",
+  id: 88163813,
+  avatar: "https://avatars.githubusercontent.com/u/88163813?v=4",
+  name: "Anastasia Bardier",
+  company: "",
+  blog: "",
+  location: "Kyiv",
+  bio: "",
+  twitter: "",
+  repos: 9,
+  followers: 0,
+  following: 0,
+  created: "2021-07-29T19:53:58Z",
+};
